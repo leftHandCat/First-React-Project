2193f61d15ab25344c71f35a8ae762a8029099f2
leftHandCat <msniu33@163.com>
2017-04-28 12:36:56 +0800

    first react

---
+++ config/apis.js
@@ -0,0 +1,8 @@
+{
+	"undefinedrankingsAnalysisPages/getCityRankings":"getCityRankings.json",
+	"undefinedrankingsAnalysisPages/getTrendLine":"getTrendLine.json",
+	"undefinedrankingsAnalysisPages/dataQuery":"dataQuery.json",
+	"undefinedcommInit/getServerTime":"getServerTime.json",
+	"undefinedrankingsAnalysisPages/listDataQuery":"listDataQuery.json",
+	"undefinedrankingsAnalysisPages/dataExport":"dataExport.json"
+}
